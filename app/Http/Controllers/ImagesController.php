2c13ca503680209c70a0d5a95691aecd66e02618
nayirerdem <you@example.com>
2018-06-20 00:52:11 +0300

    Project Added

---
+++ app/Http/Controllers/ImagesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ImagesController extends Controller
+{
+    public function upload(Request $request)
+    {
+        if(count($request->images))
+        {
+            foreach($request->images as $image)
+            {
+                $image->store('images');
+            }
+        }
+
+        return response()->json([
+            "message" => "Done"
+        ]);
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+    <head>
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Uploader</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="{{ asset('css/app.css' ) }}">
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            <div class="content" id="app">
+                <app/> 
+            </div>
+        </div>
+        <script src="{{ asset('/js/app.js') }}"></script> 
+    </body>
+</html>
